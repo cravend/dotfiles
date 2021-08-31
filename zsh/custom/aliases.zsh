a95fdbd22799bc445ec36e973aaeca514f55ca03
Dalton Craven <20dalton00@gmail.com>
2021-08-31 14:45:43 +0200

    Add custom Git aliases

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -2,3 +2,10 @@ alias gca="git commit --amend"
 alias ls="exa -F --icons"
 alias exa="exa -F --icons"
 alias vim="nvim"
+
+# Git
+alias gl="git log --graph"
+alias glo="git log --graph --oneline"
+alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+alias glgd="git log --graph --color --date=relative --stat"
+
