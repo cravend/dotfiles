1120b067254f90b036886b94755e41900c373635
Dalton Craven <7117993+cravend@users.noreply.github.com>
2026-03-19 17:06:02 +0100

    sync

---
+++ zsh/custom/functions.zsh
@@ -0,0 +1,43 @@
+dock-lock-position() {
+  defaults write com.apple.Dock position-immutable -bool true
+  killall Dock
+}
+
+dock-unlock-position() {
+  defaults write com.apple.Dock position-immutable -bool false
+  killall Dock
+}
+
+dock-lock-size() {
+  defaults write com.apple.Dock size-immutable -bool true
+  killall Dock
+}
+
+dock-unlock-size() {
+  defaults write com.apple.Dock size-immutable -bool false
+  killall Dock
+}
+
+dock-lock-contents() {
+  defaults write com.apple.Dock contents-immutable -bool true
+  killall Dock
+}
+
+dock-unlock-contents() {
+  defaults write com.apple.Dock contents-immutable -bool false
+  killall Dock
+}
+
+dock-lock() {
+  defaults write com.apple.Dock position-immutable -bool true
+  defaults write com.apple.Dock size-immutable -bool true
+  defaults write com.apple.Dock contents-immutable -bool true
+  killall Dock
+}
+
+dock-unlock() {
+  defaults write com.apple.Dock position-immutable -bool false
+  defaults write com.apple.Dock size-immutable -bool false
+  defaults write com.apple.Dock contents-immutable -bool false
+  killall Dock
+}
