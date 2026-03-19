718ef817e8121ea7ccaf4ff47c44cce09601cc51
Dalton Craven <7117993+cravend@users.noreply.github.com>
2026-03-19 15:47:27 +0100

    sync

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,13 +1,10 @@
-alias gca="git commit --amend"
 alias ls="eza -F -a"
 alias vim="nvim"
 
 # Git
+alias gca="git commit --amend"
 alias gl="git log --graph"
 alias glo="git log --graph --oneline"
-alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
-alias glgd="git log --graph --color --date=relative --stat"
-alias gfc="git diff-tree --no-commit-id --name-only -r master..HEAD | sed 's/^frontend\///'"
-alias gfco="git diff-tree --no-commit-id --name-only -r master..HEAD | sed 's/^frontend\///' | tr '\n' ' '"
-alias gfcof="gfco | sed 's/yarn.lock//' | sed 's/package.json//'"
+alias glg="git log --graph --color --date=relative --stat"
+alias glgo="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 
--- zsh/custom/functions.zsh
+++
@@ -1,8 +0,0 @@
-function chpwd() {
-  [[ -n "$CURSOR_AGENT" ]] && return
-  eza
-}
-
-function timemachinethrottle() {
-  sudo sysctl "debug.lowpri_throttle_enabled=$1"
-}
