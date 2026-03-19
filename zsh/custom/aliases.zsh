feb42c3a8443a81269b71676e994664e131c677c
Dalton Craven <7117993+cravend@users.noreply.github.com>
2026-03-19 17:07:11 +0100

    sync

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -3,6 +3,7 @@ alias vim="nvim"
 
 # Git
 alias gca="git commit --amend"
+alias gbm="git branch --merged | grep -Ev '(\*|^\+|master|main|dev)' | xargs --no-run-if-empty git branch -d"
 alias gl="git log --graph"
 alias glo="git log --graph --oneline"
 alias glg="git log --graph --color --date=relative --stat"
